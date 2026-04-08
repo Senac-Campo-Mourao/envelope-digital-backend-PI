342a0fbd7812fda9feae81fa8d7f917b588b5e15
Pedro Manoel Rosa Domingues Santos <pedromanoel0509@gmail.com>
2026-04-08 11:37:48 -0300

    Preparação para o cloud run

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,14 +1,23 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
-});
+};
+
+// Se houver um Socket Path (produção no GCP), use-o. 
+// Caso contrário, use o Host (desenvolvimento local).
+if (process.env.INSTANCE_CONNECTION_NAME) {
+  dbConfig.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+} else {
+  dbConfig.host = process.env.DB_HOST;
+}
+
+const pool = mysql.createPool(dbConfig);
 
 module.exports = pool;
